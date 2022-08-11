eb909e5723f62323a3427450c9666ddea83ff6e9
laiskaSiili <marc.folini@gmail.com>
2022-08-11 04:23:34 +0200

    Another attempt to fix static files

--- app/entrypoint_django_prod.sh
+++ app/entrypoint_django_prod.sh
@@ -3,7 +3,8 @@ set -e
 conda activate $ENV_PREFIX
 
 # Copy nginx conf explicity to shared volume
-cp /home/app/nginx/default.conf /home/app/nginx_mount
+rm -f /home/app/nginx_mount/default.conf
+cp /home/app/nginx/nginx.conf /home/app/nginx_mount
 
 # apply migrations
 python manage.py migrate
